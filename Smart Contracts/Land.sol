133c418559ecc4385d61f7c52dc5f6b3dd92e7b1
Sowbharnika <59678694+sowbarani-k@users.noreply.github.com>
2021-01-04 16:02:59 +0530

    Smart contract uploaded

---
+++ Smart Contracts/Land.sol
@@ -0,0 +1,146 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+
+/**
+ * @title        Land
+ * @author       K.Sowbharnika
+ * Created       25.12.2020
+ * Updated       03.01.2021
+ */
+
+contract Lands is ERC721{
+   
+     mapping(uint => Land) LandList;
+
+    /** Struct for Land Details */
+    struct Land{
+       
+     
+      uint256 Land_Id;        /* Land Unique Token ID */
+     
+      string Land_Type;             /* Land Type */
+     
+      string Land_Loc;              /* Land Location */
+     
+      string Land_Loc_Type;         /* Land Location Type  */
+     
+      uint256 Land_Size;            /* Land Size */
+     
+      string Land_Bound_Det;         /*Land Boundary Details*/
+     
+      uint256 Land_Patta_No;        /* Land Patta Number */
+     
+      uint256 Land_Doc_No;          /*Land Document Number*/
+     
+      address Land_Owner_Adr;       /* Land Owner Address */
+     
+     
+    }
+      Land lan;
+
+    /**
+     * @dev Create the Token by Passing the Name and Symbol to the ERC721 Constructor
+     */
+    constructor() ERC721("LandToken","KCTL") public {
+        lan.Land_Id                 = 0;
+    }
+
+    /** 
+     * @dev Function to issue a Land
+     * @param ui_Land_Type                 Land Type
+     * @param ui_Land_Loc                  Land Location 
+     * @param ui_Land_Loc_Type             Land Location Type
+     * @param ui_Land_Size                 Land Size          
+     * @param ui_Land_Bound_Det            Land Boundary Details        
+     * @param ui_Land_Patta_No             Land Patta Number
+     * @param ui_Land_Doc_No               Land Document Number
+     * @param ui_Land_Owner_Adr            Land Owner Address
+    */
+ 
+
+   
+    function issueLand(string memory ui_Land_Type, string memory ui_Land_Loc, string memory ui_Land_Loc_Type, uint256 ui_Land_Size, string memory ui_Land_Bound_Det, uint256 ui_Land_Patta_No,
+                        uint256 ui_Land_Doc_No, address ui_Land_Owner_Adr) public {
+                             
+                             lan.Land_Id                 += 1;
+                             lan.Land_Type               = ui_Land_Type;
+                             lan.Land_Loc                = ui_Land_Loc;
+                             lan.Land_Loc_Type           = ui_Land_Loc_Type;
+                             lan.Land_Size               = ui_Land_Size;
+                             lan.Land_Bound_Det          = ui_Land_Bound_Det;
+                             lan.Land_Patta_No           = ui_Land_Patta_No;
+                             lan.Land_Doc_No             = ui_Land_Doc_No;
+                             lan.Land_Owner_Adr          = ui_Land_Owner_Adr;
+                             
+                             _mint(ui_Land_Owner_Adr, lan.Land_Id);
+                             LandList[lan.Land_Id]         = lan;
+   
+       
+    }
+    /** 
+     * @dev Function to check a Land
+     * @param Land_Id                   Land Unique Token ID
+     * @param Land_Type                 Land Type
+     * @param Land_Loc                  Land Location 
+     * @param Land_Loc_Type             Land Location Type
+     * @param Land_Size                 Land Size          
+     * @param Land_Bound_Det            Land Boundary Details        
+     * @param Land_Patta_No             Land Patta Number
+     * @param Land_Doc_No               Land Document Number
+     * @param Land_Owner_Adr            Land Owner Address
+    */
+   
+   
+
+   
+    function retrieveLand(uint256 Land_Id) public view returns ( string memory Land_Type, string memory Land_Loc, string memory Land_Loc_Type, uint256 Land_Size,
+    string memory Land_Bound_Det, uint256 Land_Patta_No, uint256 Land_Doc_No, address Land_Owner_Adr) {
+       
+      Land storage Lid = LandList[Land_Id];  
+       return(
+              Lid.Land_Type,
+              Lid.Land_Loc,
+              Lid.Land_Loc_Type,
+              Lid.Land_Size,
+              Lid.Land_Bound_Det,
+              Lid.Land_Patta_No,
+              Lid.Land_Doc_No,
+              Lid.Land_Owner_Adr
+             
+              );
+       
+    }
+    
+    /** 
+     * @dev Function to modify already issued Land
+     * @param ui_Land_Type                 Land Type
+     * @param ui_Land_Loc                  Land Location 
+     * @param ui_Land_Loc_Type             Land Location Type
+     * @param ui_Land_Size                 Land Size          
+     * @param ui_Land_Bound_Det            Land Boundary Details        
+     * @param ui_Land_Patta_No             Land Patta Number
+     * @param ui_Land_Doc_No               Land Document Number
+     * @param ui_Land_Owner_Adr            Land Owner Address
+    */
+    
+    function updateLand(uint256 ui_Land_Token, string memory ui_Land_Type, string memory ui_Land_Loc, string memory ui_Land_Loc_Type, uint256 ui_Land_Size, string memory ui_Land_Bound_Det, uint256 ui_Land_Patta_No,
+                        uint256 ui_Land_Doc_No, address ui_Land_Owner_Adr) public {
+                             
+                            Land storage Lid = LandList[ui_Land_Token];  
+       
+                             Lid.Land_Type               = ui_Land_Type;
+                             Lid.Land_Loc                = ui_Land_Loc;
+                             Lid.Land_Loc_Type           = ui_Land_Loc_Type;
+                             Lid.Land_Size               = ui_Land_Size;
+                             Lid.Land_Bound_Det          = ui_Land_Bound_Det;
+                             Lid.Land_Patta_No           = ui_Land_Patta_No;
+                             Lid.Land_Doc_No             = ui_Land_Doc_No;
+                             Lid.Land_Owner_Adr          = ui_Land_Owner_Adr;
+    
+                            LandList[ui_Land_Token]  = Lid;
+    
+        
+    }
+   
+}
