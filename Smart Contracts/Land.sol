6ab4a3e1b65f0579f5e07ef0c077ca7b97d54d0a
Sowbharnika <59678694+sowbarani-k@users.noreply.github.com>
2021-03-02 10:48:00 +0530

    Update SPDX  license identifier

--- Smart Contracts/Land.sol
+++ Smart Contracts/Land.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.4.22 <0.7.0;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
